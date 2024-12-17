903e1d023f3023f0d6fa65d96c273b060a121d75
rop7 <rop@mail.ropsoft.cloud>
2024-12-17 14:44:45 -0300

    update

---
+++ script/14-install-multiple-dev-library.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Multiple libs
+sudo apt install -y libqt6*
+sudo apt install -y libxcb*
+sudo apt install -y libx11*
+sudo apt install -y libxkb*
+sudo apt install -y libwayland*
+sudo apt install -y libinput*
+sudo apt install -y libkf6con*
+
+exit 0;
