d6d0b7085fd7b2f6889805f59dda46ade8b09b51
rop7 <rop@mail.ropsoft.cloud>
2024-12-21 16:11:28 -0300

    update

---
+++ @scripts/deploy/projectSelfCopyCubic.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ..;
+
+pwd;
+
+rm -rf /home/zzyo/Desktop/rsp-isolinux/src/custom-root/home/dev/rsp;
+cp -rf rsp /home/zzyo/Desktop/rsp-isolinux/src/custom-root/home/dev;
+
+exit 0
